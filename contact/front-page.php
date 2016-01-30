667aa28f2ad430a12602c389394c06a5f1b9f4fd
keitahirai <keitah74@gmail.com>
2016-01-30 19:28:34 +0900

    front-page 追加

---
+++ contact/front-page.php
@@ -0,0 +1,5 @@
+<?php get_template_part('header_top'); ?>
+<h3 class="h-lv3-new"><i class="fa fa-calendar-check-o"></i> 過去7日間の新作一覧</h3>
+<section id="inner-new" class="fixHeight">
+<?php get_template_part('loop_new'); ?>
+<!-- /#inner-new --></section>
